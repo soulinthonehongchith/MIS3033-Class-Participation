50ab401292de6f95a10ff664df03fcbb59b7fd1c
Sou Hongchith <soulinthonehongchith@ou.edu>
2019-02-06 11:39:17 -0600

    Started the code and now stop, will come back soon

---
+++ WPF1/WPF1/Form.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class Form
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        //Constructor
+        public Form()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public Form (string name, string address, int zipCode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            string outPut = $"{Name}'s address is {Address}, {ZipCode}";
+            return outPut;
+        }
+
+    }
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Form a = new Form();
+            a.Name = txtName.Text;
+            a.Address = txtAddress.Text;
+            a.ZipCode = Convert.ToInt32(txtZipCode.Text);
+
+            MessageBox.Show(a.ToString());
+
+            ListBox.Items.Add(a);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
