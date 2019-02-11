caefff57e752d383173a9ac669df78f3a9f539f8
Sou Hongchith <soulinthonehongchith@ou.edu>
2019-02-11 11:23:40 -0600

    Additional materials

--- WPF1/WPF1/MainWindow.xaml.cs
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Soulinthone Hongchith
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,13 +30,37 @@ namespace WPF1
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
             Form a = new Form();
+            bool isFormValid = true;
             a.Name = txtName.Text;
             a.Address = txtAddress.Text;
             a.ZipCode = Convert.ToInt32(txtZipCode.Text);
 
+            //check if the user doesn't input the name
+            if (a.Name == "" || a.Name == string.Empty)
+            {
+                MessageBox.Show("Please input your name");
+                txtName.Focus();
+                isFormValid = false;
+            }
+
+            if(a.Address == "")
+            {
+                MessageBox.Show("Please input your address");
+                isFormValid = false;
+            }
+
+            if(isFormValid == false)
+            {
+                return;
+            }
+
             MessageBox.Show(a.ToString());
 
             ListBox.Items.Add(a);
+
+            txtName.Clear();
+            txtAddress.Clear();
+            txtZipCode.Clear();
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
